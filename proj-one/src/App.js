3adee68f61b51abe251dd69948cbe5b0e2b1e9cd
bitdern <acnewell09@gmail.com>
2023-01-27 15:40:04 -0600

    working on multiple components and props practice

--- proj-one/src/App.js
+++ proj-one/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { useState } from "react";
+import Title from "./components/Title";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser live stream", id: 2 },
     { title: "race on moo moo farm", id: 3 },
   ]);
+
+  console.log(showEvents);
 
   const handleClick = (id) => {
     setEvents((prevEvents) => {
@@ -17,18 +21,32 @@ function App() {
     console.log(id);
   };
 
+  const subtitle = "the latest and greatest events widdit";
+
   return (
     <div className="App">
-      {events.map((event, index) => (
-        <div key={event.id}>
-          <h2>
-            {index} - {event.title}
-          </h2>
-          <button onClick={() => handleClick(event.id)}>
-            delete event widdit
-          </button>
+      <Title title="events in your hood" subtitle={subtitle} />
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>
+              delete event widdit
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
