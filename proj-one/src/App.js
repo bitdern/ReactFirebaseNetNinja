07002d37db5a501a5f0db7ed9342d64017a7cdb0
bitdern <acnewell09@gmail.com>
2023-01-28 11:14:21 -0600

    modals and portals

--- proj-one/src/App.js
+++ proj-one/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import Title from "./components/Title";
+import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
@@ -18,7 +20,11 @@ function App() {
         return id !== event.id;
       });
     });
-    console.log(id);
+    console.log(showModal);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
   };
 
   const subtitle = "the latest and greatest events widdit";
@@ -39,15 +45,24 @@ function App() {
       )}
       {showEvents &&
         events.map((event, index) => (
-          <div key={event.id}>
+          <React.Fragment key={event.id}>
             <h2>
               {index} - {event.title}
             </h2>
             <button onClick={() => handleClick(event.id)}>
               delete event widdit
             </button>
-          </div>
+          </React.Fragment>
         ))}
+
+      {showModal && (
+        <Modal handleClose={handleClose}>
+          <h2>WAT u mean?!?</h2>
+          <p>enter code "blintz" to recieve de la RIEN!</p>
+        </Modal>
+      )}
+
+      <button onClick={() => setShowModal(true)}>where dat modal at doe</button>
     </div>
   );
 }
