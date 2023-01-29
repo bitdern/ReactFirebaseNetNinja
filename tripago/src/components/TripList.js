47c6d42c362162f837b655ef4dfe2a39c98b943f
bitdern <acnewell09@gmail.com>
2023-01-29 17:21:09 -0600

    tripago project

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,30 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useFetch } from "../hooks/usefetch";
 
 //styles
 import "./TripList.css";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/trips")
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
-  }, []);
-
-  console.log(trips);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+  const { data: trips, isPending, error } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>TripList</h2>
+      {isPending && <div>Loading Trips...</div>}
+      {error && <div>{error}</div>}
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 }
