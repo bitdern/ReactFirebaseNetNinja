3598874d185b73fb8cdc5c8285fe2156539fe2a1
bitdern <acnewell09@gmail.com>
2023-02-02 16:17:31 -0600

    more set up for recipe project

--- cooking-ninja/src/App.js
+++ cooking-ninja/src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// page components
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+
+//styles
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
