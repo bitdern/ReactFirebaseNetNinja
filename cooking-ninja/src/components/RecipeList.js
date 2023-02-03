7b81239f872cf595585f93c81594ee94fef97b71
bitdern <acnewell09@gmail.com>
2023-02-03 08:48:19 -0600

    imported useFetch and made a recipe list component

---
+++ cooking-ninja/src/components/RecipeList.js
@@ -0,0 +1,15 @@
+//styles
+import "./RecipeList.css";
+
+export default function RecipeList({ recipes }) {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="card">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
