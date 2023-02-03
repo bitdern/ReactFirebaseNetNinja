07c5694f323c1b773a65169ee077b160269979b9
bitdern <acnewell09@gmail.com>
2023-02-03 10:33:36 -0600

    added route params + useFetch to recipe details page

--- cooking-ninja/src/components/RecipeList.js
+++ cooking-ninja/src/components/RecipeList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 //styles
 import "./RecipeList.css";
 
@@ -7,7 +9,9 @@ export default function RecipeList({ recipes }) {
       {recipes.map((recipe) => (
         <div key={recipe.id} className="card">
           <h3>{recipe.title}</h3>
-          <p>{recipe.cookingTime}</p>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
         </div>
       ))}
     </div>
--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,6 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/usefetch";
+
 // styles
 import "./Recipe.css";
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+  const history = useNavigate();
+
+  useEffect(() => {
+    if (error) {
+      //redirect
+      setTimeout(() => {
+        history.push("/");
+      }, 2000);
+    }
+  }, [error, history]);
+
+  return (
+    <div className="recipe">
+      {isPending && <div className="loading">Loading...</div>}
+      {error && <div className="error">{error}</div>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
+  );
 }
