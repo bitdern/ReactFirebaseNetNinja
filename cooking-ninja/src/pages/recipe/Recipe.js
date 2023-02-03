3a09901092a2d2b2e4fde2ed222b0260e47d398d
bitdern <acnewell09@gmail.com>
2023-02-03 11:02:01 -0600

    added create recipe page

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/usefetch";
 
 // styles
@@ -9,22 +8,23 @@ export default function Recipe() {
   const { id } = useParams();
   const url = "http://localhost:3000/recipes/" + id;
   const { data: recipe, isPending, error } = useFetch(url);
-  const history = useNavigate();
-
-  useEffect(() => {
-    if (error) {
-      //redirect
-      setTimeout(() => {
-        history.push("/");
-      }, 2000);
-    }
-  }, [error, history]);
 
   return (
     <div className="recipe">
       {isPending && <div className="loading">Loading...</div>}
       {error && <div className="error">{error}</div>}
-      {recipe && <h1>{recipe.title}</h1>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
   );
 }
