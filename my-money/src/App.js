e30c5826fef99b69fa42bc3d3d11242e2a62231f
bitdern <acnewell09@gmail.com>
2023-02-20 15:45:17 -0600

    home, login and sign up pages plus router functionality

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// page components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
