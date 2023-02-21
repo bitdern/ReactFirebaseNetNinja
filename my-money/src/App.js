0609c66656b477f357c1f5d101d10d94c79e4c89
bitdern <acnewell09@gmail.com>
2023-02-21 16:00:40 -0600

    clean up

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-import Navbar from "./pages/components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
