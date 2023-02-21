4d1d87e4d2191d4ccf8e840db8d3a1eb94d9bae5
bitdern <acnewell09@gmail.com>
2023-02-21 15:50:12 -0600

    added sign up form

--- my-money/src/pages/signup/Signup.js
+++ my-money/src/pages/signup/Signup.js
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
+// styles
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, userName, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Sign Up!</h2>
+      <label>
+        <span>
+          email:
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </span>
+      </label>
+      <label>
+        <span>user name:</span>
+        <input
+          type="text"
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
+  );
 }
