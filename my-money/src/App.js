ad9a8cf8d0fbde80dbd85abe538f2d47c2140d8c
bitdern <acnewell09@gmail.com>
2023-02-21 15:07:40 -0600

    navbar and links to login and signup pages

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./pages/components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
