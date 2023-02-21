b931fe3fd7c55ae4287672dcfa707bf0f820f529
bitdern <acnewell09@gmail.com>
2023-02-21 15:59:23 -0600

    set up a firebase backend and enabled user auth

---
+++ my-money/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvXKC2j1JhoRHZUWM02qbedSQOC62p5WA",
+  authDomain: "mymoney-eb972.firebaseapp.com",
+  projectId: "mymoney-eb972",
+  storageBucket: "mymoney-eb972.appspot.com",
+  messagingSenderId: "244712849023",
+  appId: "1:244712849023:web:7ca708d44a28503027494a",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
